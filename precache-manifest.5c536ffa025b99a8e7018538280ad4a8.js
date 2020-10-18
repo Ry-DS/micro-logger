b670cedad20e271edad1a5c784986e8b2eb3fabd
Ryan <ryan2002.rs@gmail.com>
2020-10-18 18:18:14 +1100

    Updates

---
+++ precache-manifest.5c536ffa025b99a8e7018538280ad4a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498d9c8e43b6f548d8eb8ae3dd3dd756",
+    "url": "/micro-logger/index.html"
+  },
+  {
+    "revision": "2b67824892c2a9137b3a",
+    "url": "/micro-logger/static/css/2.7b1a92e0.chunk.css"
+  },
+  {
+    "revision": "b2d99716486b225882d5",
+    "url": "/micro-logger/static/css/main.591faf1a.chunk.css"
+  },
+  {
+    "revision": "2b67824892c2a9137b3a",
+    "url": "/micro-logger/static/js/2.16fc7b32.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/micro-logger/static/js/2.16fc7b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d99716486b225882d5",
+    "url": "/micro-logger/static/js/main.b64566a4.chunk.js"
+  },
+  {
+    "revision": "7ab02920ebd74bd856df",
+    "url": "/micro-logger/static/js/runtime-main.ddd36211.js"
+  },
+  {
+    "revision": "3227be1b883b5c3c9c2917ff4451102e",
+    "url": "/micro-logger/static/media/mb_simplified.3227be1b.stl"
+  }
+]);
